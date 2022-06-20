9fe977929445baeed2b5efd6671b874da9d2eeef
ReCodeLife <recodelife@proton.me>
2022-06-20 09:26:10 +0800

    Complete file reading

--- src/App.vue
+++ src/App.vue
@@ -9,20 +9,21 @@
       </el-header>
       <el-main style="padding-top: 0">
 
-        <DataTable v-if="activeName==='citation'"/>
-        <CitationStyle v-else-if="activeName==='style'" />
+        <DataTable v-if="activeName === 'citation'"  :tableData="tableData"/>
+        <CitationStyle v-else-if="activeName === 'style'" />
 
 
       </el-main>
-      <el-footer >
-        <div v-if="activeName==='citation'">
-          <el-button type="primary" plain >Add Citation</el-button>
+      <el-footer>
+        <div v-if="activeName === 'citation'">
+          <el-button type="primary" plain>Add Citation</el-button>
           <el-button type="primary" plain>refresh</el-button>
           <el-button type="primary" plain>Add Bibliography</el-button>
-          <el-button type="primary" plain>Select bib file</el-button>
+          <el-button type="primary" plain @click.native="selectFile">Select bib file</el-button>
+          <input type="file" style="display:none" id="FileButton" @change="filetest" accept=".bib"/>
         </div>
         <div v-else>
-          <el-button type="primary" plain >get more style</el-button>
+          <el-button type="primary" plain>get more style</el-button>
         </div>
 
       </el-footer>
@@ -34,55 +35,123 @@
 import DataTable from "@/components/DataTable";
 import CitationStyle from "@/components/CitationStyle";
 const Cite = require('citation-js')
+const bibtexParse = require('bibtex-parse');
+
 export default {
   name: 'App',
-  components:{DataTable,CitationStyle},
-  data(){
-    return{
-      activeName:"citation"
+  components: { DataTable, CitationStyle },
+  data() {
+    return {
+      activeName: "citation",
+      tableData:[]
     }
   },
   methods: {
-    // onSetColor() {
-    //   window.Word.run(async context => {
-    //     const range = context.workbook.getSelectedRange();
-    //     range.format.fill.color = 'green';
-    //     await context.sync();
-    //   });
-    // }
+    
+    selectFile() {
+      const fileInput = document.getElementById("FileButton");
+      // fileInput.onclick = () => {
+      //   var file = fileInput.files[0];
+      //   console.log(file)
+      //   const reader = new FileReader();
+      //   var data = {};
+      //   reader.onload = () => {
+      //     var fileData = reader.result;
+      //     var cite = new Cite(fileData, '@bibtex/text');
+      //     data = cite.get();
+      //     console.log(data)
+      //   };
+      //   reader.readAsText(file);
+      // }
+      fileInput.click();
+
+
+      //return data;
+    },
+    filetest(e){
+      console.log('change');
+      //console.log(e)
+      const fileInput=e.srcElement;
+      if(fileInput.files.length==0){
+        console.log("no select file")
+        return ;
+      }
+        var file = fileInput.files[0];
+        console.log(file)
+        const reader = new FileReader();
+        var data = {};
+        reader.onload = () => {
+          var fileData = reader.result;
+          var cite = new Cite(fileData,  { "forceType": '@biblatex/text' });
+          data = cite.get();
+          console.log(data)
+          //console.log(cite.data)
+           this.tableData=data
+          // var data=bibtexParse.entries(fileData);
+          // console.log(data)
+          // this.tableData=data
+
+        };
+        reader.readAsText(file);
+    }
   },
   mounted() {
     //const Cite = require('citation-js')
-    var cite=new Cite("@InProceedings{汪2021,\n" +
-        "  author     = {汪, 森 and 冷, 枭 and 柯, 灿 and 周, 红},\n" +
-        "  title      = {乘用车驾驶风格设计优化研究},\n" +
-        "  year       = {2021},\n" +
-        "  note       = {1 citations(CNKI)[2022-5-9]},\n" +
-        "  pages      = {200--206},\n" +
-        "  publisher  = {中国汽车工程学会（China Society of Automotive Engineers）},\n" +
-        "  abstract   = {驾驶风格定义了车辆的动态响应,表征了驾驶员对驾乘车辆的驾驶预期。驾驶风格基于车辆的加速响应主观体现,主要由加速踏板曲线和换档策略决定。通过市场定位及竞品分析,定义开发车型的宏观加速响应目标,继而设计并优化加速踏板曲线及换档曲线、对比修正加速响应曲线,开展主观评价及客观测试,综合平衡动力总成性能,可以设计出满足客户真实需求的驾驶风格,达成市场预期。},\n" +
-        "  annote     = {hello what’s your name},\n" +
-        "  doi        = {10.26914/c.cnkihy.2021.027834},\n" +
-        "  file       = {:汪2021 - 乘用车驾驶风格设计优化研究.pdf:PDF},\n" +
-        "  groups     = {主观拟合},\n" +
-        "  keywords   = {换档策略, 加速踏板曲线, 加速响应, 驾驶风格, 性能合同},\n" +
-        "  language   = {中文;},\n" +
-        "  readstatus = {skimmed},\n" +
-        "  url        = {https://kns.cnki.net/KCMS/detail/detail.aspx?dbcode=CPFD&dbname=CPFDLAST2021&filename=QCGC202110003040&v=},\n" +
-        "}",{"forceType":'@bibtex/text'});
-    console.log(cite)
-    console.log(cite.get())
+    //console.log(cite)
+    //console.log(cite.get())
+//     let example = new Cite('Q21972834')
+//     console.log(example.get())
+// // Should implicitly display 
+// console.log(example.format('bibliography', {
+//   format: 'html',
+//   template: 'citation-apa',
+//   lang: 'en-US'
+// }))
+// const { plugins } = require('@citation-js/core')
+// const config = plugins.config.get("@bibtex")
+// console.log(Cite.config)
+
+// console.log(config
+// )
+// config.constants.required.bibtex.inproceedings.push("abstract")
+// console.log(config.constants.required.bibtex.inproceedings)
+
+// var cite=Cite(`@Article{Kim2021,
+//   author     = {Kim, Dongchan and Shon, Hyukju and Kweon, Nahyun and Choi, Seungwon and Yang, Chanuk and Huh, Kunsoo},
+//   journal    = {Ieee Access},
+//   title      = {Driving {Style}-{Based} {Conditional} {Variational} {Autoencoder} for {Prediction} of {Ego} {Vehicle} {Trajectory}},
+//   year       = {2021},
+//   issn       = {2169-3536},
+//   note       = {WOS:000736733700001},
+//   pages      = {169348--169356},
+//   volume     = {9},
+//   abstract   = {Trajectory prediction of the ego vehicle is essential for advanced driver assistance systems to function properly. By recognizing various driving styles and predicting trajectories reflecting them, the prediction performance is enhanced, and a personalized trajectory can be generated. Therefore, we propose to combine driving style recognition and trajectory prediction tasks using only in-vehicle CAN-bus sensor data for possible application to normal vehicles. The DeepConvLstm network was utilized for driving style recognition, and a generative-based model was used for trajectory prediction. The classified driving style was added as a condition to the network. In addition, the past trajectory of the ego vehicle is estimated and utilized as an additional input for performance improvement. The performance of the proposed method is analyzed in terms of the root mean squared error (RMSE) and mean absolute error (MAE) compared with the case wherein the driving style and the past trajectory are not conditioned or given, respectively. The results demonstrate the effectiveness of the proposed method.},
+//   address    = {Piscataway},
+//   doi        = {10.1109/ACCESS.2021.3138502},
+//   file       = {:Kim2021 - Driving Style Based Conditional Variational Autoencoder for Prediction of Ego Vehicle Trajectory.pdf:PDF},
+//   groups     = {编码器, 引文},
+//   keywords   = {Trajectory, Feature extraction, Vehicles, Training, Global Positioning System, Data models, Task analysis, Driving style recognition, trajectory prediction, conditional variational autoencoder, CAN data, Hardware-in-the-Loop simulation},
+//   language   = {English},
+//   priority   = {prio1},
+//   publisher  = {Ieee-Inst Electrical Electronics Engineers Inc},
+//   ranking    = {rank4},
+//   readstatus = {skimmed},
+//   url        = {https://www.webofscience.com/wos/alldb/full-record/WOS:000736733700001},
+//   urldate    = {2022-06-04},
+// }`,{ "forceType": '@biblatex/text' })
+// console.log(cite.get())
   }
 };
 </script>
 
 <style>
-
-.el-header, .el-footer {
+.el-header,
+.el-footer {
   text-align: center;
 
 }
-.el-header > .el-tabs--top >.el-tabs__header {
+
+.el-header>.el-tabs--top>.el-tabs__header {
   margin: 0;
 }
 
@@ -99,13 +168,13 @@ export default {
 
 }
 
-body > .el-container {
+body>.el-container {
   margin-bottom: 40px;
 }
-html,body{
+
+html,
+body {
   height: 100%;
   margin: 0;
 }
-
-
 </style>
--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -23,15 +23,19 @@
           </el-table-column>
           <el-table-column
               label="日期"
-              width="120">
-            <template slot-scope="scope">{{ scope.row.date }}</template>
+              width="120"
+               
+              >
+            <template slot-scope="scope">{{ scope.row.issued?scope.row.issued['date-parts'][0][0] : "" }}</template>
+           
           </el-table-column>
           <el-table-column
-              prop="author"
+              
               label="作者"
               width="120"
               show-overflow-tooltip
           >
+          <template slot-scope="scope">{{ authorToString(scope.row.author) }}</template>
           </el-table-column>
           <el-table-column
               prop="title"
@@ -53,78 +57,86 @@
 <script>
 export default {
   name: "DataTable",
+  props: {
+    tableData: {
+      default: () => {
+        return [];
+      },
+    },
+    //loading: {default:false},
+  },
   data() {
 
     return {
-      tableData: [{
-        date: '2016-05-03',
-        author: '王小虎xxxxxxxxx',
-        title: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-02',
-        author: '王小虎',
-        title: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-04',
-        author: '王小虎',
-        title: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-01',
-        author: '王小虎',
-        title: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-08',
-        author: '王小虎',
-        title: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-06',
-        author: '王小虎',
-        title: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-07',
-        author: '王小虎',
-        title: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-04',
-        author: '王小虎',
-        title: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-01',
-        author: '王小虎',
-        title: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-08',
-        author: '王小虎',
-        title: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-06',
-        author: '王小虎',
-        title: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-07',
-        author: '王小虎',
-        title: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-04',
-        author: '王小虎',
-        title: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-01',
-        author: '王小虎',
-        title: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-08',
-        author: '王小虎',
-        title: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-06',
-        author: '王小虎',
-        title: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-07',
-        author: '王小虎',
-        title: '上海市普陀区金沙江路 1518 弄'
-      }],
+      // tableData: [{
+      //   date: '2016-05-03',
+      //   author: '王小虎xxxxxxxxx',
+      //   title: '上海市普陀区金沙江路 1518 弄'
+      // }, {
+      //   date: '2016-05-02',
+      //   author: '王小虎',
+      //   title: '上海市普陀区金沙江路 1518 弄'
+      // }, {
+      //   date: '2016-05-04',
+      //   author: '王小虎',
+      //   title: '上海市普陀区金沙江路 1518 弄'
+      // }, {
+      //   date: '2016-05-01',
+      //   author: '王小虎',
+      //   title: '上海市普陀区金沙江路 1518 弄'
+      // }, {
+      //   date: '2016-05-08',
+      //   author: '王小虎',
+      //   title: '上海市普陀区金沙江路 1518 弄'
+      // }, {
+      //   date: '2016-05-06',
+      //   author: '王小虎',
+      //   title: '上海市普陀区金沙江路 1518 弄'
+      // }, {
+      //   date: '2016-05-07',
+      //   author: '王小虎',
+      //   title: '上海市普陀区金沙江路 1518 弄'
+      // }, {
+      //   date: '2016-05-04',
+      //   author: '王小虎',
+      //   title: '上海市普陀区金沙江路 1518 弄'
+      // }, {
+      //   date: '2016-05-01',
+      //   author: '王小虎',
+      //   title: '上海市普陀区金沙江路 1518 弄'
+      // }, {
+      //   date: '2016-05-08',
+      //   author: '王小虎',
+      //   title: '上海市普陀区金沙江路 1518 弄'
+      // }, {
+      //   date: '2016-05-06',
+      //   author: '王小虎',
+      //   title: '上海市普陀区金沙江路 1518 弄'
+      // }, {
+      //   date: '2016-05-07',
+      //   author: '王小虎',
+      //   title: '上海市普陀区金沙江路 1518 弄'
+      // }, {
+      //   date: '2016-05-04',
+      //   author: '王小虎',
+      //   title: '上海市普陀区金沙江路 1518 弄'
+      // }, {
+      //   date: '2016-05-01',
+      //   author: '王小虎',
+      //   title: '上海市普陀区金沙江路 1518 弄'
+      // }, {
+      //   date: '2016-05-08',
+      //   author: '王小虎',
+      //   title: '上海市普陀区金沙江路 1518 弄'
+      // }, {
+      //   date: '2016-05-06',
+      //   author: '王小虎',
+      //   title: '上海市普陀区金沙江路 1518 弄'
+      // }, {
+      //   date: '2016-05-07',
+      //   author: '王小虎',
+      //   title: '上海市普陀区金沙江路 1518 弄'
+      // }],
       multipleSelection: []
     }
   },
@@ -141,7 +153,15 @@ export default {
     },
     handleSelectionChange(val) {
       this.multipleSelection = val;
-    }
+    },
+    authorToString(authors){
+      //var s="";
+      //console.log(authors)
+      const a=authors.map((author)=>author.family+author.given)
+      return a.join();
+    },
+    
+
   }
 }
 </script>
